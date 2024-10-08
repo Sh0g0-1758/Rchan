89116f48445af358626358b5f3582388cf6da12b
Sh0g0-1758 <shouryagoel10000@gmail.com>
2024-10-08 11:28:19 +0530

    Implement wrapping integers

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -4,16 +4,21 @@ using namespace std;
 
 Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 {
-  // Your code here.
-  (void)n;
-  (void)zero_point;
-  return Wrap32 { 0 };
+  return Wrap32((n + zero_point.raw_value_) % (1UL << 32));
 }
 
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
-  // Your code here.
-  (void)zero_point;
-  (void)checkpoint;
-  return {};
+  uint64_t divisor = 1UL << 32;
+  uint64_t abs_seqno = raw_value_ >= zero_point.raw_value_ 
+                     ? raw_value_ - zero_point.raw_value_ 
+                     : divisor + raw_value_ - zero_point.raw_value_;
+
+  if (checkpoint <= abs_seqno) return abs_seqno;
+  uint64_t diff = checkpoint - abs_seqno;
+  uint64_t quotient = diff / divisor;
+  uint64_t remainder = diff % divisor;
+  uint64_t k = quotient + (remainder >= (divisor / 2) ? 1 : 0);
+
+  return abs_seqno + k * divisor;
 }
