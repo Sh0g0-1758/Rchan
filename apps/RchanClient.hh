879f21b0cb5943279b7e5ab807b1b0d213346f83
Sh0g0-1758 <shouryagoel10000@gmail.com>
2024-11-07 22:18:38 +0530

    fmt

--- apps/RchanClient.hh
+++ apps/RchanClient.hh
@@ -4,34 +4,34 @@
 
 #include "local_server.cc"
 
-class RchanClient {
+class RchanClient
+{
 private:
-    std::atomic<bool> running;
-    std::string username;
-    std::unique_ptr<RchanSocket> RsockPtr;
-    std::string RchanServerIP;
-    std::unique_ptr<std::thread> RchanClientPtr;
-    std::map<std::string, std::string> servers;
-    std::mutex socketMutex;
-    std::string fragment_store;
-    std::unique_ptr<LocalServer> localServerPtr;
+  std::atomic<bool> running;
+  std::string username;
+  std::unique_ptr<RchanSocket> RsockPtr;
+  std::string RchanServerIP;
+  std::unique_ptr<std::thread> RchanClientPtr;
+  std::map<std::string, std::string> servers;
+  std::mutex socketMutex;
+  std::string fragment_store;
+  std::unique_ptr<LocalServer> localServerPtr;
 
-    void listenForMessages(std::unique_ptr<RchanSocket>& sock);
-    std::pair<std::string, int> parseIpPort(const std::string& address);
+  void listenForMessages( std::unique_ptr<RchanSocket>& sock );
+  std::pair<std::string, int> parseIpPort( const std::string& address );
 
 public:
-    RchanClient();
-    ~RchanClient();
+  RchanClient();
+  ~RchanClient();
 
-    // Delete copy constructor and assignment operator due to unique_ptr members
-    RchanClient(const RchanClient&) = delete;
-    RchanClient& operator=(const RchanClient&) = delete;
+  RchanClient( const RchanClient& ) = delete;
+  RchanClient& operator=( const RchanClient& ) = delete;
 
-    void EnterServer(std::string server_name);
-    void HostServer();
-    void unHostServer();
-    void getUserName();
-    void getChatHistory();
-    void sendMessage(std::string message);
-    void getServers();
+  void EnterServer( std::string server_name );
+  void HostServer();
+  void unHostServer();
+  void getUserName();
+  void getChatHistory();
+  void sendMessage( std::string message );
+  void getServers();
 };
--- apps/RchanServer.hh
+++ apps/RchanServer.hh
@@ -2,44 +2,46 @@
 
 #include "Rchan_req.hpp"
 
-struct server_info {
-    std::string server_name;
-    std::string server_ip;
-    int server_port;
-    std::string root_password;
-    std::string server_password;
+struct server_info
+{
+  std::string server_name;
+  std::string server_ip;
+  int server_port;
+  std::string root_password;
+  std::string server_password;
 };
 
-class RchanServer {
+class RchanServer
+{
 private:
-    static const int PORT = 8080;
-    static const int BUFFER_SIZE = 4096;
+  static const int PORT = 8080;
+  static const int BUFFER_SIZE = 4096;
 
-    std::vector<int> clients;
-    std::map<std::string, std::string> servers;
-    std::map<std::string, server_info> localRChanServers;
-    std::mutex clientsMutex;
-    std::vector<std::string> usernames;
-    
-    int server_fd;
-    int new_socket;
-    sockaddr_in address;
-    int addrlen;
-    int opt;
+  std::vector<int> clients;
+  std::map<std::string, std::string> servers;
+  std::map<std::string, server_info> localRChanServers;
+  std::mutex clientsMutex;
+  std::vector<std::string> usernames;
 
-    std::string hashPSWD(const std::string& input);
-    void handleClient(int clientSocket);
-    void sendMessage(const nlohmann::json& message, int clientSocket);
-    void sendChatHistory(int clientSocket);
-    void sendAvailableServers(int clientSocket);
-    void writeMessageToFile(const std::string& message);
+  int server_fd;
+  int new_socket;
+  sockaddr_in address;
+  int addrlen;
+  int opt;
+
+  std::string hashPSWD( const std::string& input );
+  void handleClient( int clientSocket );
+  void sendMessage( const nlohmann::json& message, int clientSocket );
+  void sendChatHistory( int clientSocket );
+  void sendAvailableServers( int clientSocket );
+  void writeMessageToFile( const std::string& message );
+
 public:
-    RchanServer();
-    ~RchanServer();
+  RchanServer();
+  ~RchanServer();
 
-    // Delete copy constructor and assignment operator
-    RchanServer(const RchanServer&) = delete;
-    RchanServer& operator=(const RchanServer&) = delete;
+  RchanServer( const RchanServer& ) = delete;
+  RchanServer& operator=( const RchanServer& ) = delete;
 
-    void Run();
+  void Run();
 };
